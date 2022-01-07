0ed97b8999caabfa747de21b77ea094d943dc603
Mark Mooibroek <mark@appnormal.com>
2022-01-07 12:07:50 +0100

    Base with working puzzle

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_challenge/models/tile.dart';
+import 'package:flutter_challenge/state/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Challenge 2022',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Column(
+          children: const [
+            SizedBox(height: 20),
+            Text('Flutter Challenge'),
+            Expanded(
+              child: Center(
+                child: FractionallySizedBox(
+                  widthFactor: 0.8,
+                  heightFactor: 0.8,
+                  child: Center(child: _PuzzleView()),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PuzzleView extends ConsumerWidget {
+  const _PuzzleView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final puzzleState = ref.watch(puzzleProvider);
+    return GridView.count(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: puzzleState.puzzle.getDimension(),
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      children: puzzleState.puzzle.tiles.map((e) => _Tile(e)).toList(),
+    );
+  }
+}
+
+class _Tile extends StatelessWidget {
+  const _Tile(this.tile, {Key? key}) : super(key: key);
+
+  final Tile tile;
+
+  @override
+  Widget build(BuildContext context) {
+    return tile.isWhitespace ? const SizedBox.shrink() : _TappableTile(tile);
+  }
+}
+
+class _TappableTile extends ConsumerWidget {
+  const _TappableTile(this.tile, {Key? key}) : super(key: key);
+
+  final Tile tile;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () => ref.read(puzzleViewModel).tileTapped(tile),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text("${tile.value}"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/state/providers.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_challenge/state/puzzle_viewmodel.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'puzzle_state.dart';
+
+// Inital puzzle state
+final puzzleProvider = StateProvider((_) => PuzzleState.initial(3));
+
+// Provides the viewmodel that is in charge of changing the puzzle state
+final puzzleViewModel = Provider((ref) => PuzzleViewModel(ref.read));
