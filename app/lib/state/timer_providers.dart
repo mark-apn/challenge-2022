e5a299fcc6521e617b65080a7f18352dfb89933d
Mark Mooibroek <mark@appnormal.com>
2022-01-23 20:34:54 +0100

    Update created at timer display

--- app/lib/state/timer_providers.dart
+++ app/lib/state/timer_providers.dart
@@ -8,8 +8,11 @@ final createdAtTime = StateProvider.autoDispose((ref) => "...");
 final createdAtTimerProvider = Provider.autoDispose<void>((ref) {
   final createdAt = ref.watch(puzzleProvider.select((value) => value.puzzle.createdAt));
 
-  String toTimeString(int time1, int time2) {
-    return "${time1.toString().padLeft(2, '0')}:${time2.toString().padLeft(2, '0')}";
+  String toTimeString(int time1, int time2, [int? time3]) {
+    if (time3 == null) {
+      return "${time1.toString().padLeft(2, '0')}:${time2.toString().padLeft(2, '0')}";
+    }
+    return "${time1.toString().padLeft(2, '0')}:${time2.toString().padLeft(2, '0')}:${time3.toString().padLeft(2, '0')}";
   }
 
   final timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -18,9 +21,11 @@ final createdAtTimerProvider = Provider.autoDispose<void>((ref) {
 
     final String displayTime;
     if (diff.inMinutes < 60) {
-      displayTime = toTimeString(diff.inMinutes, diff.inSeconds);
+      displayTime = toTimeString(diff.inMinutes, diff.inSeconds % 60);
     } else if (diff.inHours < 24) {
-      displayTime = toTimeString(diff.inHours, diff.inMinutes);
+      displayTime = toTimeString(diff.inHours, diff.inMinutes % 60, diff.inSeconds % 60);
+    } else if (diff.inDays == 1) {
+      displayTime = "1 day";
     } else {
       displayTime = "${diff.inDays} days";
     }
